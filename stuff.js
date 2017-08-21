8bd9f490633346bebe76c2512bc32ae927a4dc3c
turquoise-turtle <a3j2p1@gmail.com>
2017-08-21 17:41:47 +1000

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,33 @@
+function closeTheBoxModalNowOrElse() {
+	var se = document.getElementById("openModal");
+	se.parentNode.removeChild(se);
+}
+function fontbombactivate() {
+	var s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src = "https://gitcdn.xyz/repo/turquoise-turtle/reimagined-giggle/master/fontbomb.js";
+	document.body.appendChild(s);
+}
+function frenchtoastactivate () {
+	var t = document.createElement("script");
+	t.type = "text/javascript";
+	t.src = "https://gitcdn.xyz/repo/turquoise-turtle/reimagined-giggle/master/frenchtoast.js";
+	document.body.appendChild(t);
+}
+function xraygogglesactivate () {
+    var u = document.createElement("script");
+    u.src = "https://goggles.mozilla.org/webxray.js";
+    u.className = "webxray";
+    u.setAttribute("data-lang", "en-US");
+    u.setAttribute("data-baseuri", "https://goggles.mozilla.org");
+    document.body.appendChild(u);
+}
+function kathackactivate () {
+	var v = document.createElement("script");
+	v.src = "https://gitcdn.xyz/repo/turquoise-turtle/reimagined-giggle/master/kathack.js";
+	v.type = "text/javascript";
+	var w = document.createElement("script");
+	w.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js";
+	w.type = "text/javascript";
+	document.body.appendChild(v, w);
+}
