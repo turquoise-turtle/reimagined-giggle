a3e62a9e6253b7c95c6d4264d85f35c0029e9d21
turquoise-turtle <a3j2p1@gmail.com>
2017-11-23 06:33:33 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-  var mecss = document.createElement('style');
+  	var mecss = document.createElement('style');
 	mecss.innerHTML = '.activateMessy,.closeMessy{color:#FFF !important;text-decoration:none !important;font-weight:700}.modalDialogMessy{position:fixed;font-family:sans-serif;top:0;right:0;bottom:0;left:0;background:rgba(0,0,0,.8);opacity:1; z-index:10000}.modalDialogMessy>div{width:400px;position:relative;margin:10% auto;padding:5px 20px 13px;border-radius:10px;background:#fff}.closeMessy{background:#606061;line-height:25px;position:absolute;right:-12px;text-align:center;top:-10px;width:24px;border-radius:12px}.closeMessy:hover{background:#dc143c}.activateMessy{color:#fff;background:#000}.activateMessy:hover{background:#b8860b}.aTwoMessy:hover{background:#00d9ff}.aThreeMessy:hover{background:#3cb371}a:hover{color:#fff}';
 	document.head.appendChild(mecss);
 	var mescriptyscript = document.createElement('script');
@@ -7,9 +7,3 @@
 	var dcontainer = document.createElement('div');
 	dcontainer.innerHTML = '<div id="openModal" class="modalDialogMessy"><div><a href="#close" onclick="closeTheBoxModalNowOrElse();" title="Close" class="closeMessy">X</a><h1>Mess up the page!</h1><h3><a class="activateMessy" href="#" onclick="closeTheBoxModalNowOrElse();fontbombactivate();" id="fontbomb">FontBomb</a></h3><h3><a class="activateMessy aTwoMessy" href="#" onclick="closeTheBoxModalNowOrElse();frenchtoastactivate();">French Toast</a></h3><h3><a class="activateMessy aThreeMessy" href="#" onclick="closeTheBoxModalNowOrElse();xraygogglesactivate();">X-Ray Goggles</a></h3><h3><a class="activateMessy" href="#" onclick="closeTheBoxModalNowOrElse();kathackactivate();">KatHack</a></h3><h3><a class="activateMessy aTwoMessy" href="#" onclick="closeTheBoxModalNowOrElse();kickassactivate();">Asteroids</a></h3></div></div>';
 	document.body.appendChild(dcontainer);
-var newLinksArea = document.getElementsByName("footer")[0];
-var newLinks = document.createElement("a");
-newLinks.style = "text-decoration:none;color:#182736";
-newLinks.href = "https://via.hypothes.is/" + location.href;
-newLinks.innerText = "gg";
-newLinksArea.appendChild(newLinks);
